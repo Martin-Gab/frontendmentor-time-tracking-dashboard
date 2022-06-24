71c9d4c4bde385c10b8a233bd716d9765650bec9
Martin-Gab <martingabillet@gmail.com>
2022-06-24 16:00:35 +0200

    project setup

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// =======================
+// ==== App Component ====
+// =======================
+
+// ======================
+// ==== Local Import ====
+// ======================
+import './app.scss';
+
+const App = () => (
+  <div className="app">
+    Ici les composants de l'App
+  </div>
+);
+
+export default App;
