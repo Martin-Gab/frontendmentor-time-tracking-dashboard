39578770bc3afdadc58d1b7b044abe2a4d3a4873
Martin-Gab <martingabillet@gmail.com>
2022-06-24 17:00:04 +0200

    JSX and CSS for perso card OK

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,10 +6,21 @@
 // ==== Local Import ====
 // ======================
 import './app.scss';
+import picture from '../../assets/images/image-jeremy.png';
 
 const App = () => (
   <div className="app">
-    Ici les composants de l'App
+    <div className="perso-card">
+      <div className="infos">
+        <img src={picture} alt="Jeremy's profile picture" className="infos__picture" />
+        <p className="infos__name"><span>Report for</span>Jeremy Robson</p>
+      </div>
+      <div className="selection">
+        <div className="selection__item">Daily</div>
+        <div className="selection__item--selected">Weekly</div>
+        <div className="selection__item">Monthly</div>
+      </div>
+    </div>
   </div>
 );
 
