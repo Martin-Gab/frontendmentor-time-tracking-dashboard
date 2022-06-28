0e3c229fac67a018bb43c8fd803af1a796617abc
Martin-Gab <martingabillet@gmail.com>
2022-06-28 14:36:30 +0200

    JSX and CSS OK for card

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -2,8 +2,28 @@
 // ==== Card Sub-Component ====
 // ============================
 
+// ======================
+// ==== Local Import ====
+// ======================
+import menu from '../../assets/images/icon-ellipsis.svg';
+import workIcon from '../../assets/images/icon-work.svg';
+
 const Card = () => (
-  <div>Ici le composant Card</div>
+  <div className="card">
+    <div className="card-icon">
+      <img src={workIcon} alt="Work Icon" className="card-icon__picture" />
+    </div>
+    <div className="card-content">
+      <div className="top">
+        <h2 className="top__title">Work</h2>
+        <div className="top__menu" />
+      </div>
+      <div className="bottom">
+        <div className="bottom__hours">32hrs</div>
+        <div className="bottom__before">Last Week - 36hrs</div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Card;
