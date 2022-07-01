26e1ff22ba61e219b378aa1984669f728123f9e6
Martin-Gab <martingabillet@gmail.com>
2022-07-01 10:09:28 +0200

    dynamic display of hours on timeframe selection OK

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -5,6 +5,7 @@
 // ======================
 // ==== Local Import ====
 // ======================
+import { useSelector } from "react-redux";
 
 const Card = ({ data }) => {
   // === Function to apply the right background color to the right card ===
@@ -41,6 +42,51 @@ const Card = ({ data }) => {
     return cssClass;
   }
 
+  // === Values from the timeframeReducer ===
+  const dailySelected = useSelector((state) => state.timeframe.dailySelected);
+  const weeklySelected = useSelector((state) => state.timeframe.weeklySelected);
+  const monthlySelected = useSelector((state) => state.timeframe.monthlySelected);
+
+  // === Function to fetch the right values from data according to the selected timeframe ===
+  // === Current timeframe ===
+  const selectedTimeframeCurrent = () => {
+    if (dailySelected) {
+      return data.timeframes.daily.current;
+    }
+    else if (weeklySelected) {
+      return data.timeframes.weekly.current;
+    }
+    else if (monthlySelected) {
+      return data.timeframes.monthly.current;
+    }
+  }
+
+  // === Previous timeframe ===
+  const selectedTimeframePrevious = () => {
+    if (dailySelected) {
+      return data.timeframes.daily.previous;
+    }
+    else if (weeklySelected) {
+      return data.timeframes.weekly.previous;
+    }
+    else if (monthlySelected) {
+      return data.timeframes.monthly.previous;
+    }
+  }
+
+  // === Function to display correct timeframe text content on card ===
+  const timeframeTextContent = () => {
+    if (dailySelected) {
+      return 'Day';
+    }
+    else if (weeklySelected) {
+      return 'Week';
+    }
+    else if (monthlySelected) {
+      return 'Month';
+    }
+  }
+
   return (
     <div className={cardCssClass(data.title)}>
       <div className="card-icon">
@@ -52,8 +98,8 @@ const Card = ({ data }) => {
           <div className="top__menu" />
         </div>
         <div className="bottom">
-          <div className="bottom__hours">{`${data.timeframes.weekly.current}hrs`}</div>
-          <div className="bottom__before">{`Last Week - ${data.timeframes.weekly.previous}hrs`}</div>
+          <div className="bottom__hours">{`${selectedTimeframeCurrent()}hrs`}</div>
+          <div className="bottom__before">{`Last ${timeframeTextContent()} - ${selectedTimeframePrevious()}hrs`}</div>
         </div>
       </div>
     </div>
